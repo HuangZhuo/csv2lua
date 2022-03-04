5907b527167c9478ddef85f22452d2e92f15004f
huangzhuo <idorable@163.com>
2022-03-04 17:35:35 +0800

    add all test related files

--- test_custom.py
+++ test_custom.py
@@ -4,5 +4,5 @@ import custom
 
 class TestCustom(unittest.TestCase):
     def test_mongen(self):
-        custom.mongen('csv/地图对应NPC.csv', 'csv/mongen.csv')
-        custom.write_bosstrial('csv/mongen_tmp.csv', 'csv/bosstrial.csv')
+        custom.mongen('csv_custom/地图对应NPC.csv', 'csv_custom/mongen.csv')
+        custom.write_bosstrial('csv_custom/mongen_tmp.csv', 'csv_custom/bosstrial.csv')
--- test_idsub.py
+++ test_idsub.py
@@ -4,4 +4,4 @@ import idsub
 
 class TestIDSub(unittest.TestCase):
     def test_main(self):
-        idsub.process('./csv/caiquanrankaward.csv')
+        idsub.process('./csv_idsub/caiquanrankaward.csv')
